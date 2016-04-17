41bbc224cf520b1c4c34376b67a785ce0bf2b6bc
Nazrul Kamaruddin <nazroll@gmail.com>
2016-04-18 03:27:49 +0800

    Initial version of FB bot following the getting started guide.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# gilacoolbot
+# GilaCool Bot for Messenger
+This project is a guide for developers on using [Google Cloud Platform](http://cloud.google.com) to build a bot service for [Facebook Messenger Platform](https://developers.facebook.com/docs/messenger-platform).
+
+Infrastructure stack:
+
+- Google App Engine
+
+Application stack:
+
+- Flask-RESTful
---
+++ main/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse, abort
+import logging
+from google.appengine.api import urlfetch
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+FB_PAGE_ACCESS_TOKEN = ""
+
+parser = reqparse.RequestParser()
+parser.add_argument('hub.mode')
+parser.add_argument('hub.challenge')
+parser.add_argument('hub.verify_token')
+
+def fb_send_text_message(sender, text):
+    fb_sender_id = sender['id']
+    fb_sender_text = text
+
+    reply = {
+        'recipient': {
+            'id': fb_sender_id
+        },
+        'message': {
+            'text': fb_sender_text
+        }
+    }
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    payload = json.dumps(reply)
+    url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + FB_PAGE_ACCESS_TOKEN
+    req = urlfetch.fetch(
+        url,
+        payload,
+        urlfetch.POST,
+        headers
+    )
+    logging.debug(req.content)
+
+
+class FbMessengerBotHandler(Resource):
+    def get(self):
+        if args['hub.verify_token']:
+            return int(args['hub.challenge'])
+        return 'ok'
+
+    def post(self):
+        json = request.get_json()
+        logging.debug(json)
+        if json:
+            fb_messaging = json['entry'][0]['messaging']
+            for fb_message in fb_messaging:
+                sender = fb_message['sender']
+                text = fb_message['message']['text']
+                fb_send_text_message(sender, text)
+
+api.add_resource(FbMessengerBotHandler, '/fbmessenger')
+
+if __name__ == '__main__':
+    app.run(debug=True)
